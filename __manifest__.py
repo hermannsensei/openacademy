f8950f9993287b5b4cecb449ec9498ce078d6925
hermannsensei <hermannsensei@gmail.com>
2019-02-12 12:53:18 +0100

    Premier commit

---
+++ __manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "openacademy",
+
+    'summary': """
+        Manage training courses""",
+
+    'description': """
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base',
+        'board',
+        'mail',
+    ],
+
+    # always loaded
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/polo.xml',
+        'views/partner.xml',
+        'views/session_board.xml',
+        'reports.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ models/models.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+from datetime import timedelta
+from odoo import models, fields, api, exceptions, _
+
+class openacademy(models.Model):
+    _name = 'openacademy.openacademy'
+
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        self.value2 = float(self.value) / 100
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = "OpenAcademy Courses"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+    responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many(
+        'openacademy.session', 'course_id', string="Sessions")
+
+    @api.multi
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', _(u"Copy of {}%").format(self.name))])
+        if not copied_count:
+            new_name = _(u"Copy of {}").format(self.name)
+        else:
+            new_name = _(u"Copy of {} ({})").format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Course, self).copy(default)
+
+    _sql_constraints = [
+        ('name_description_check',
+         'CHECK(name != description)',
+         "The title of the course should not be the description"),
+
+        ('name_unique',
+         'UNIQUE(name)',
+         "The course title must be unique"),
+    ]
+
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+    _description = "OpenAcademy Sessions"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    name = fields.Char(required=True,track_visibility='onchange')
+    start_date = fields.Date(default=fields.Date.today)
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats",track_visibility='onchange')
+    active = fields.Boolean(default=True)
+    color = fields.Integer()
+    instructor_id = fields.Many2one('res.partner', string="Instructor",
+         domain=['|', ('instructor', '=', True),
+                ('category_id.name', 'ilike', "Teacher")],track_visibility='onchange')
+
+    course_id = fields.Many2one('openacademy.course',
+                                ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    add_date = fields.Datetime(string = 'Adding Date',default=fields.Datetime.today)
+
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    end_date = fields.Datetime(string="End Date", store=True,
+                           compute='_get_end_date', inverse='_set_end_date')
+
+    attendees_count = fields.Integer(
+        string="Attendees count", compute='_get_attendees_count', store=True)
+    state = fields.Selection([
+        ('draft', "Draft"),
+        ('confirmed', "Confirmed"),
+        ('done', "Done"),
+    ], default='draft',track_visibility='onchange')
+
+
+    @api.multi
+    def action_draft(self):
+        self.state = 'draft'
+
+    @api.multi
+    def action_confirm(self):
+        self.state = 'confirmed'
+
+    @api.multi
+    def action_done(self):
+        self.state = 'done'
+
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        for r in self:
+            if not r.seats:
+                r.taken_seats = 0.0
+            else:
+                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+
+    @api.onchange('seats', 'attendee_ids')
+    def _verify_valid_seats(self):
+        if self.seats < 0:
+            return {
+                'warning': {
+                    'title': _("Incorrect 'seats' value"),
+                    'message': _("The number of available seats may not be negative"),
+                },
+            }
+        if self.seats < len(self.attendee_ids):
+            return {
+                'warning': {
+                    'title': _("Too many attendees"),
+                    'message': _("Increase seats or remove excess attendees"),
+                },
+            }
+
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+
+            # Add duration to start_date, but: Monday + 5 days = Saturday, so
+            # subtract one second to get on Friday instead
+            start = fields.Datetime.from_string(r.start_date)
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = start + duration
+
+    def _set_end_date(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+
+            # Compute the difference between dates, but: Friday - Monday = 4 days,
+            # so add one day to get 5 days instead
+            start_date = fields.Datetime.from_string(r.start_date)
+            end_date = fields.Datetime.from_string(r.end_date)
+            r.duration = (end_date - start_date).days + 1
+
+    @api.depends('attendee_ids')
+    def _get_attendees_count(self):
+        for r in self:
+            r.attendees_count = len(r.attendee_ids)
+
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _check_instructor_not_in_attendees(self):
+        for r in self:
+            if r.instructor_id and r.instructor_id in r.attendee_ids:
+                raise exceptions.ValidationError(_("A session's instructor can't be an attendee"))
+
+    # def add_follower_id(self, res_id, model, partner_id):
+    #     follower_id = False
+    #     reg = {
+    #         'res_id': res_id,
+    #         'res_model': model,
+    #         'partner_id': partner_id
+    #     }
+    #     try:
+    #         follower_id = self.env['mail.followers'].create(reg)
+    #     except:
+    #         # This partner is already following this record
+    #         return False
+    #     return follower_id
+    #
+    # @api.multi
+    # def write(self, vals):
+    #     res = super(Session, self).write(vals)  # Save the form
+    #     stage_followers = self.env['openacademy.session'].search([('state', '=' , 'done')])
+    #     for i in stage_followers:
+    #         self.add_follower_id(self, self.id, 'session.form', i['state'])
+    #     # Message posting is optional. Add_follower_id will still make the partner follow the record
+    #     messages = "Whatever you want to put in the message box."
+    #     if messages:
+    #         self.message_post(body=messages, partner_ids=self.message_follower_ids)
+    #     return res
+    #
